caba0828a36c9e87e8c1e930f245e0a066c115f7
Christian Metzner <metznerchristian97@gmail.com>
2025-06-05 23:39:16 +0200

    added dendritic delay option

--- mnist_runs.sh
+++ mnist_runs.sh
@@ -1,5 +1,5 @@
-python main.py --dataset 'mnist' --gpu 1 --conv_mode 'dcls' --seed 0
-python main.py --dataset 'mnist' --gpu 1 --conv_mode 'dcls' --seed 1
-python main.py --dataset 'mnist' --gpu 1 --conv_mode 'dcls' --seed 2
-python main.py --dataset 'mnist' --gpu 1 --conv_mode 'dcls' --seed 3
-python main.py --dataset 'mnist' --gpu 1 --conv_mode 'dcls' --seed 4
+python main.py --dataset 'mnist' --gpu 1 --conv_mode 'vanilla' --seed 0
+python main.py --dataset 'mnist' --gpu 1 --conv_mode 'vanilla' --seed 1
+python main.py --dataset 'mnist' --gpu 1 --conv_mode 'vanilla' --seed 2
+python main.py --dataset 'mnist' --gpu 1 --conv_mode 'vanilla' --seed 3
+python main.py --dataset 'mnist' --gpu 1 --conv_mode 'vanilla' --seed 4
