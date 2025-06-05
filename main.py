ff7ea4a9bcffc00c9b2e13d27a22ba94079ebbb7
Tristan Torchet <ttorchet@ini.uzh.ch>
2025-06-05 15:58:31 +0200

    experiments_cifar - yaml file to run sweeps manually

--- main.py
+++ main.py
@@ -228,11 +228,12 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Train a GRU model")
     parser.add_argument("--dataset", type=str, default="mnist", choices=['mnist', 'cifar'], help="Dataset version: mnist or cifar")
     parser.add_argument("--gpu", type=int, default=0, help="GPU to use")
-    parser.add_argument("--conv_mode", type=str, default="dcls", choices=['dcls', 'conv_eerf', 'conv_lerf', 'vanilla'], help="Convolution mode: dcls, causal_eerf, or causal_lerf")
+    parser.add_argument("--conv_mode", type=str, default="dcls", choices=['dcls', 'conv_eerf', 'conv_lerf', 'vanilla', 'tcn_lerf'], help="Convolution mode: dcls, causal_eerf, or causal_lerf")
+    parser.add_argument("--file_nb", type=int, default=0, help="File number to load the configuration from")
     args_cli = parser.parse_args()
 
     def parse_args():
-        with open(f"{args_cli.dataset}_{args_cli.conv_mode}.yaml", "r") as file:
+        with open(f"{args_cli.dataset}_{args_cli.conv_mode}_{args_cli.file_nb}.yaml", "r") as file:
             config = yaml.safe_load(file)
         return argparse.Namespace(**config)
 
